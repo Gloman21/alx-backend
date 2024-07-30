ef1afeb355f5d95f8ac4580d54c0dc38ca029685
Gloman21 <irankundab21ertin@gmail.com>
2024-07-30 22:38:05 +0200

    tasks

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/envpython3
+"""
+basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('0-index.html')
